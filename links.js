9d29f31e4488bfaad837a718310bc5927fbaa164
Jdelfinado <151306752+Jdelfinado@users.noreply.github.com>
2023-11-18 12:05:55 +0800

    Delete links.js

--- links.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const aboutLink = document.getElementById("about-link");
-
-    aboutLink.addEventListener("click", function (e) {
-      e.preventDefault();
-      window.location.href = "#bg-image1";
-    });
-  });
